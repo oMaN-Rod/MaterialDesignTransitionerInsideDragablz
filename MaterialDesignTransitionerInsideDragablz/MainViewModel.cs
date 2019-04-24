5b6d35388ff9023850676f7eff97e3ccb5d1518c
OmarR <orod41285@gmail.com>
2019-04-24 07:00:38 -0400

    Initial commit

---
+++ MaterialDesignTransitionerInsideDragablz/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using MaterialDesignTransitionerInsideDragablz.Annotations;
+
+namespace MaterialDesignTransitionerInsideDragablz
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Slides = new object[] { Form1, Form2, Form3 };
+        }
+
+        public ICommand PrevCommand => new DelegateCommand(Previous);
+        public ICommand NextCommand => new DelegateCommand(Next);
+
+        public object[] Slides { get; }
+
+        public SimpleFormView Form1 { get; set; } = new SimpleFormView() { Title = "Form 1" };
+
+        public SimpleFormView Form2 { get; set; } = new SimpleFormView() { Title = "Form 2" };
+
+        public SimpleFormView Form3 { get; set; } = new SimpleFormView() { Title = "Form 3" };
+
+        private int _activeSlideIndex;
+        public int ActiveSlideIndex
+        {
+            get => _activeSlideIndex;
+            set
+            {
+                _activeSlideIndex = value;
+                OnPropertyChanged(nameof(ActiveSlideIndex));
+            }
+        }
+
+        private void Previous()
+        {
+            if (ActiveSlideIndex == 0)
+            {
+                ActiveSlideIndex = Slides.Length - 1;
+            }
+            else
+            {
+                ActiveSlideIndex--;
+            }
+        }
+
+        private void Next()
+        {
+            if (ActiveSlideIndex == Slides.Length - 1)
+            {
+                ActiveSlideIndex = 0;
+            }
+            else
+            {
+                ActiveSlideIndex++;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MaterialDesignTransitionerInsideDragablz/SimpleFormView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignTransitionerInsideDragablz.Annotations;
+
+namespace MaterialDesignTransitionerInsideDragablz
+{
+    public partial class SimpleFormView : INotifyPropertyChanged
+    {
+        public SimpleFormView()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Title { get; set; }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Submit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Message = $"Hello {FirstName} {LastName}, have a great day!";
+        }
+    }
+}
